8ef9c83237f7f72861fe5fe8464519994feba283
Kiran Karkera <kiran.karkera@gmail.com>
2016-08-01 00:35:11 +0800

    load raw file path instead of from resources

--- test/clojure_word2vec/core_test.clj
+++ test/clojure_word2vec/core_test.clj
@@ -1,7 +1,13 @@
 (ns clojure-word2vec.core-test
   (:require [clojure.test :refer :all]
-            [clojure-word2vec.core :refer :all]))
+            [clojure-word2vec.core :refer :all]
+            [clojure.java.io :as io]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "generate a dataset"
+    (let [data (create-input-format (io/resource "ulysses.txt"))
+          model  (word2vec data)]
+    (is (= '("The" "Project" "Gutenberg" "EBook" "of" "Ulysses," "by" "James" "Joyce")
+           (first data)))
+    (is (= '("all" "were" "so" "this" "one" "him" "like" "And" "I" "as"))
+        (get-matches model "woman")))))
